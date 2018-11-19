c1ddc31de55d52056dd80fe5178f98fc65a27c0d
Antony Chan <antony.chan@oocl.com>
2018-11-19 17:47:12 +0800

    Refactoring

--- main/main.js
+++ main/main.js
@@ -1,30 +1,51 @@
 module.exports = function main(input) {
-	var item_map = [];
-	var item_index = [];
-	var total_charge = 0;
+
+
+	let item_map = initializeItemMap(input);
+	var total_charge = getTotalCharge(input);
 	var output = "";
 
 	output += '***<没钱赚商店>购物清单***\n';
-
-	input.forEach(function(item){
-		if (item_map[item.barcode] == undefined) {
-			var item_record = {name: item.name, unit: item.unit, count: 1, price: item.price.toFixed(2)};
-			item_map[item.barcode] = item_record;
-			item_index.push(item.barcode);
-		} else {
-			item_map[item.barcode].count++;
-		}
-	});
-
-	for (var i = 0; i < item_index.length; i++) {
-		var item_record = item_map[item_index[i]];
-		output += '名称：' + item_record.name + '，数量：' + item_record.count + item_record.unit + '，单价：' + item_record.price + '(元)，小计：' + (item_record.price * item_record.count).toFixed(2) + '(元)\n';
-		total_charge += item_record.price * item_record.count;
-	};
-
+	output += decomposeItemListToOutput(item_map);
 	output += '----------------------\n';
 	output += '总计：' + total_charge.toFixed(2) + '(元)\n';
 	output += '**********************';
 
 	return output;
 };
+
+function initializeItemMap(input){
+	var item_map = [];
+	input.forEach(function(item){
+		if (item_map[item.barcode] == undefined) {
+			let item_record = {name: item.name, unit: item.unit, count: 1, price: item.price.toFixed(2)};
+			item_map[item.barcode] = item_record;
+		} else {
+			item_map[item.barcode].count++;
+		}
+	});
+	return item_map;
+}
+
+function getTotalCharge(input){
+	var total_charge = 0;
+	input.forEach(item => total_charge += item.price);
+	return total_charge;
+}
+
+function decomposeItemListToOutput(item_map){
+	var output = "";
+
+	for (const key in item_map) {
+		item_record = item_map[key];
+		output += generateItemRecordString(item_record);
+	};
+
+	return output;
+}
+
+function generateItemRecordString(item_record){
+	return '名称：' + item_record.name + '，数量：' + item_record.count + item_record.unit + '，单价：' + item_record.price + '(元)，小计：' + (item_record.price * item_record.count).toFixed(2) + '(元)\n';
+
+	// return '名称：${item_record.name}，数量：${item_record.count}${item_record.unit}，单价：${item_record.price}(元)，小计：${(item_record.price * item_record.count).toFixed(2)}(元)\n';
+}
